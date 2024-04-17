23270d93d6143e859ad55eb7c8bc82e0593e1ec4
sakhaline <alexandra.ivva@gmail.com>
2024-04-17 15:09:56 +0300

    Fix bug

--- src/main.py
+++ src/main.py
@@ -172,12 +172,12 @@ def retool_send_data() -> None:
 
 
 def main(postgres_access: bool = False) -> dict:
-    # process_campaign_list()
+    process_campaign_list()
     if postgres_access:
         logger.info(f"{main.__name__} -- MODE - POSTGRES DIRECT ACCESS")
     else:
         logger.info(f"{main.__name__} -- MODE - RETOOL API")
-        # prepare_retool_json()
+        prepare_retool_json()
         retool_send_data()
     # TODO: 
     # 5: notify ACTSE team by email about sync result (see description in Telegram)
@@ -186,6 +186,4 @@ def main(postgres_access: bool = False) -> dict:
 
 
 if __name__ == "__main__":
-    # get_weekly_campaigns()
-    # process_campaign_list()
     main()
